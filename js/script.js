f6b1a722ea6924739813c4959abe423ec5ec4f82
dykailhamp <dykailhampangestu@gmail.com>
2024-05-23 14:42:24 +0700

    third change

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-//Ini JS
+document.getElementById("hitung").addEventListener("click", function() {
+  let alas = parseFloat(document.getElementById("alas").value);
+  let tinggi = parseFloat(document.getElementById("tinggi").value);
+  
+  // Validasi input
+  if (alas > 0 && tinggi > 0){
+    let Luas = 1/2 * alas * tinggi;
+    document.getElementById('proses').innerHTML = `
+        Luas = 1/2 x Alas x Tinggi <br>
+        Luas = 1/2 x ${alas} x ${tinggi} <br>
+        Luas = 0.5 x ${alas} x ${tinggi} <br>
+        Luas = ${Luas}`
+  }else{
+    document.getElementById('Luas').innerText = "Mohon masukan angka yang valid";
+  }
+});
+
+document.getElementById("reset").addEventListener("click", function() {
+  document.getElementById("Luas").innerText = "";
+  document.getElementById("proses").innerHTML = "";
+  document.getElementById("alas").value = "";
+  document.getElementById("tinggi").value = "";
+});
